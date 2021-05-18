5532361ed6df0046efccb2d62e96d8afbc3c5a89
E Lazar <emiliosalazar4@gmail.com>
2021-05-18 18:13:45 -0500

    added rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,7 +74,15 @@ if (promptFight === "skip" || promptFight === "SKIP") {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+  
       var pickedEnemyName = enemyNames[i];
       enemyHealth = 30;
+      debugger;
     fight(pickedEnemyName);
 }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+    }
+  } 
